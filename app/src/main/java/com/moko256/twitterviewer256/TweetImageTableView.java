dda5d7707378fe544accb5debe96f8c9a2566185
moko256 <koutaro.mo@gmail.com>
2016-09-25 00:57:11 +0900

    remake TweetImageTableView

--- app/src/main/java/com/moko256/twitterviewer256/TweetImageTableView.java
+++ app/src/main/java/com/moko256/twitterviewer256/TweetImageTableView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -18,6 +17,8 @@ import twitter4j.ExtendedMediaEntity;
  */
 public class TweetImageTableView extends android.support.v7.widget.GridLayout {
 
+    private ImageView imageViews[];
+
     public TweetImageTableView(Context context) {
         this(context, null);
     }
@@ -28,31 +29,6 @@ public class TweetImageTableView extends android.support.v7.widget.GridLayout {
 
     public TweetImageTableView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-    }
-
-
-    public void setTwitterMediaEntities(ExtendedMediaEntity mediaEntities[]){
-        View view=null;
-
-        switch (mediaEntities.length){
-            case 1:
-                view= LayoutInflater.from(getContext()).inflate(R.layout.layout_tweet_image_1,this);
-                break;
-            case 2:
-                view= LayoutInflater.from(getContext()).inflate(R.layout.layout_tweet_image_2,this);
-                break;
-            case 3:
-                view= LayoutInflater.from(getContext()).inflate(R.layout.layout_tweet_image_3,this);
-                break;
-            case 4:
-                view= LayoutInflater.from(getContext()).inflate(R.layout.layout_tweet_image_4,this);
-                break;
-
-            case 0:
-            default:
-                return;
-
-        }
 
         int ids[]={
                 R.id.layout_tweet_image_1,
@@ -61,18 +37,49 @@ public class TweetImageTableView extends android.support.v7.widget.GridLayout {
                 R.id.layout_tweet_image_4
         };
 
-        for(int i=0;i<mediaEntities.length;i++){
+        LayoutInflater.from(getContext()).inflate(R.layout.layout_tweet_image_table,this);
 
-            ExtendedMediaEntity mediaEntity=mediaEntities[i];
-            String urlText=mediaEntity.getMediaURLHttps()+":orig";
+        imageViews=new ImageView[4];
 
-            ImageView imageView=(ImageView) view.findViewById(ids[i]);
-            Glide.with(getContext()).load(urlText).centerCrop().into(imageView);
-            imageView.setOnClickListener(v->{
-                Intent intent=new Intent(getContext(),ShowTweetImageActivity.class);
-                intent.putExtra("TweetMediaEntity",mediaEntity);
-                getContext().startActivity(intent);
-            });
+
+        for(int i=0;i<ids.length;i++){
+            imageViews[i]=(ImageView) findViewById(ids[i]);
+        }
+    }
+
+    public void setTwitterMediaEntities(ExtendedMediaEntity mediaEntities[]){
+
+        /* {row,column,rowSpan,colSpan} */
+        int params[][][]={
+                {{0,0,2,2},{},{},{}},
+                {{0,0,2,1},{0,1,2,1},{},{}},
+                {{0,0,2,1},{0,1,1,1},{1,1,1,1},{}},
+                {{0,0,1,1},{0,1,1,1},{1,0,1,1},{1,1,1,1}}
+        };
+
+        for(int i=0;i<4;i++){
+
+            ImageView imageView=imageViews[i];
+
+            if(i<mediaEntities.length){
+                imageView.setVisibility(VISIBLE);
+
+                ExtendedMediaEntity mediaEntity=mediaEntities[i];
+                String urlText=mediaEntity.getMediaURLHttps()+":orig";
+
+                int param[]=params[mediaEntities.length-1][i];
+
+                imageView.setLayoutParams(makeGridLayoutParams(param[0],param[1],param[2],param[3]));
+                imageView.setOnClickListener(v->{
+                    Intent intent=new Intent(getContext(),ShowTweetImageActivity.class);
+                    intent.putExtra("TweetMediaEntity",mediaEntity);
+                    getContext().startActivity(intent);
+                });
+                Glide.with(getContext()).load(urlText).centerCrop().into(imageView);
+            }else{
+                imageView.setVisibility(GONE);
+            }
+
         }
 
     }
@@ -91,5 +98,12 @@ public class TweetImageTableView extends android.support.v7.widget.GridLayout {
         int measuredHeightSpec=MeasureSpec.makeMeasureSpec(heightSize,heightMode);
 
         super.onMeasure(measuredWidthSpec, measuredHeightSpec);
+    }
+
+    private LayoutParams makeGridLayoutParams(int row,int column,int rowSpan,int colSpan){
+        LayoutParams params=new LayoutParams();
+        params.rowSpec=spec(row,rowSpan,1F);
+        params.columnSpec=spec(column,colSpan,1F);
+        return params;
     }
 }
