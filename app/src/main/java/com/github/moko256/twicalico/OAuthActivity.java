86ed32e3153b402bbc48fe78ec540548102dcdd2
moko256 <koutaro.mo@gmail.com>
2017-02-02 15:49:24 +0900

    add pin auth

--- app/src/main/java/com/github/moko256/twicalico/OAuthActivity.java
+++ app/src/main/java/com/github/moko256/twicalico/OAuthActivity.java
@@ -6,7 +6,10 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -26,82 +29,129 @@ import twitter4j.conf.ConfigurationContext;
  */
 public class OAuthActivity extends AppCompatActivity {
     public static RequestToken req;
+    public static boolean requirePin=false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_oauth);
 
         Uri uri=getIntent().getData();
-        if(uri != null && uri.toString().startsWith("twicalico://OAuthActivity")){
-            OAuthAuthorization oauth;
+        if(requirePin||(uri != null && uri.toString().startsWith("twicalico://OAuthActivity"))){
 
-            Configuration conf = ConfigurationContext.getInstance();
-            oauth =new OAuthAuthorization(conf);
-            String verifier = uri.getQueryParameter("oauth_verifier");
-            Observable
-                    .create(subscriber -> {
-                        try {
-                            subscriber.onNext(oauth.getOAuthAccessToken(req,verifier));
-                            subscriber.onCompleted();
-                        } catch (TwitterException e) {
-                            subscriber.onError(e);
-                        }
-                    })
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                            result->{
-                                AccessToken accessToken=(AccessToken) result;
-                                SharedPreferences defaultSharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
-
-                                String token = accessToken.getToken();
-                                String tokenSecret = accessToken.getTokenSecret();
-
-                                TokenSQLiteOpenHelper tokenOpenHelper = new TokenSQLiteOpenHelper(this);
-                                long nowAccountPoint=tokenOpenHelper.setAccessToken(accessToken)-1;
-                                tokenOpenHelper.close();
-
-                                defaultSharedPreferences
-                                        .edit()
-                                        .putString("AccountPoint",String.valueOf(nowAccountPoint))
-                                        .apply();
-
-                                GlobalApplication.twitter = new TwitterFactory().getInstance();
-                                GlobalApplication.twitter.setOAuthConsumer(GlobalApplication.consumerKey, GlobalApplication.consumerSecret);
-                                GlobalApplication.twitter.setOAuthAccessToken(
-                                        new AccessToken(token, tokenSecret)
-                                );
-
-                                finish();
-                                startActivity(new Intent(this,MainActivity.class));
-                            },
-                            Throwable::printStackTrace,
-                            ()->{}
-                    );
+            if (!requirePin){
+                initToken(uri.getQueryParameter("oauth_verifier"));
+            }
         }
         else {
-            Configuration conf = ConfigurationContext.getInstance();
-            final OAuthAuthorization oauth =new OAuthAuthorization(conf);
-
-            oauth.setOAuthConsumer(GlobalApplication.consumerKey,GlobalApplication.consumerSecret);
-
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                public Void doInBackground(Void... params) {
-                    try {
-                        req = oauth.getOAuthRequestToken("twicalico://OAuthActivity");
-                    } catch (TwitterException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                }
-
-                @Override
-                public void onPostExecute(Void n) {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(req.getAuthorizationURL())));
-                }
-            }.execute();
+            new AlertDialog.Builder(this)
+                    .setPositiveButton("Use URL scheme Auth",(dialog, which) -> startUrlAuth())
+                    .setNegativeButton("Use PIN Auth",(dialog, which) -> startPinAuth())
+                    .show();
         }
+    }
+
+    private void startUrlAuth(){
+        Configuration conf = ConfigurationContext.getInstance();
+        final OAuthAuthorization oauth =new OAuthAuthorization(conf);
+
+        oauth.setOAuthConsumer(GlobalApplication.consumerKey,GlobalApplication.consumerSecret);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            public Void doInBackground(Void... params) {
+                try {
+                    req = oauth.getOAuthRequestToken("twicalico://OAuthActivity");
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            public void onPostExecute(Void n) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(req.getAuthorizationURL())));
+            }
+        }.execute();
+    }
+
+    private void startPinAuth(){
+        Configuration conf = ConfigurationContext.getInstance();
+        final OAuthAuthorization oauth =new OAuthAuthorization(conf);
+        requirePin=true;
+        oauth.setOAuthConsumer(GlobalApplication.consumerKey,GlobalApplication.consumerSecret);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            public Void doInBackground(Void... params) {
+                try {
+                    req = oauth.getOAuthRequestToken("oob");
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            public void onPostExecute(Void n) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(req.getAuthorizationURL())));
+            }
+        }.execute();
+
+        EditText editText=new EditText(this);
+        editText.setInputType(EditorInfo.TYPE_NUMBER_FLAG_DECIMAL);
+        new AlertDialog.Builder(this)
+                .setView(editText)
+                .setPositiveButton(android.R.string.ok,(dialog, which) -> initToken(editText.getText().toString()))
+                .show();
+    }
+
+    private void initToken(String verifier){
+        OAuthAuthorization oauth;
+
+        Configuration conf = ConfigurationContext.getInstance();
+        oauth =new OAuthAuthorization(conf);
+
+        Observable
+                .create(subscriber -> {
+                    try {
+                        subscriber.onNext(oauth.getOAuthAccessToken(req,verifier));
+                        subscriber.onCompleted();
+                    } catch (TwitterException e) {
+                        subscriber.onError(e);
+                    }
+                })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        result-> storeAccessToken(((AccessToken) result)),
+                        Throwable::printStackTrace,
+                        ()->{}
+                );
+    }
+
+    private void storeAccessToken(AccessToken accessToken){
+        SharedPreferences defaultSharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+
+        String token = accessToken.getToken();
+        String tokenSecret = accessToken.getTokenSecret();
+
+        TokenSQLiteOpenHelper tokenOpenHelper = new TokenSQLiteOpenHelper(this);
+        long nowAccountPoint=tokenOpenHelper.setAccessToken(accessToken)-1;
+        tokenOpenHelper.close();
+
+        defaultSharedPreferences
+                .edit()
+                .putString("AccountPoint",String.valueOf(nowAccountPoint))
+                .apply();
+
+        GlobalApplication.twitter = new TwitterFactory().getInstance();
+        GlobalApplication.twitter.setOAuthConsumer(GlobalApplication.consumerKey, GlobalApplication.consumerSecret);
+        GlobalApplication.twitter.setOAuthAccessToken(
+                new AccessToken(token, tokenSecret)
+        );
+
+        finish();
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
