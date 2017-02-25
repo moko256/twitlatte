fcd19e9fb9980412f7240b66770c078c7b5cd4e9
moko256 <koutaro.mo@gmail.com>
2017-02-25 23:29:47 +0900

    add PreferenceScreen

--- app/src/main/java/com/github/moko256/twicalico/SettingsActivity.java
+++ app/src/main/java/com/github/moko256/twicalico/SettingsActivity.java
@@ -3,20 +3,24 @@ package com.github.moko256.twicalico;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 
 /**
  * Created by moko256 on 2016/03/27.
  *
  * @author moko256
  */
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartScreenCallback {
+
+    ActionBar actionBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        ActionBar actionBar=getSupportActionBar();
+        actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_back_white_24dp);
 
@@ -29,9 +33,30 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        actionBar=null;
+    }
+
+    @Override
     public boolean onSupportNavigateUp(){
         finish();
         return false;
     }
 
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen pref) {
+        PreferenceFragmentCompat fragment=new SettingsFragment();
+        Bundle args=new Bundle();
+        args.putString(PreferenceFragmentCompat.ARG_PREFERENCE_ROOT, pref.getKey());
+        fragment.setArguments(args);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setBreadCrumbTitle(pref.getTitle())
+                .setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out,android.R.anim.fade_in,android.R.anim.fade_out)
+                .replace(R.id.activity_settings_container,fragment)
+                .addToBackStack(null)
+                .commit();
+        return true;
+    }
 }
