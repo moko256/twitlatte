dc7786ce30f8738f17a3e8de4557f76acfdf886d
moko256 <koutaro.mo@gmail.com>
2017-04-18 21:55:14 +0900

    fix typo

--- app/src/main/java/com/github/moko256/twicalico/model/SendTweetModel.java
+++ app/src/main/java/com/github/moko256/twicalico/model/SendTweetModel.java
@@ -124,7 +124,7 @@ public class SendTweetModel {
 
     public static class InvalidTextException extends Throwable {
         public InvalidTextException(){
-            super("Invalid lengh of tweet text");
+            super("Invalid length of tweet text");
         }
     }
 }
