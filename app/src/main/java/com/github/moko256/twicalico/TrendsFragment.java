c01addee02af6a2e974b6270f099b4629a42addb
moko256 <koutaro.mo@gmail.com>
2017-07-06 13:49:19 +0900

    get trend location from Geocoder

--- app/src/main/java/com/github/moko256/twicalico/TrendsFragment.java
+++ app/src/main/java/com/github/moko256/twicalico/TrendsFragment.java
@@ -16,17 +16,10 @@
 
 package com.github.moko256.twicalico;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Rect;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -36,8 +29,10 @@ import android.view.ViewGroup;
 
 import com.github.moko256.twicalico.database.CachedTrendsSQLiteOpenHelper;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -135,50 +130,30 @@ public class TrendsFragment extends BaseListFragment {
 
     @Override
     protected void onInitializeList() {
-        int permissionStatus;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            permissionStatus = getActivity().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
-        } else {
-            permissionStatus = PackageManager.PERMISSION_GRANTED;
-        }
-
-        switch (permissionStatus){
-            case PackageManager.PERMISSION_GRANTED:
-                subscription.add(
-                        getLocationObservable()
-                                .concatMap(location -> getResponseObservable(location).subscribeOn(Schedulers.newThread()))
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .subscribe(
-                                        result-> {
-                                            list.clear();
-                                            list.addAll(Arrays.asList(result.getTrends()));
-                                            adapter.notifyDataSetChanged();
-                                        },
-                                        e -> {
-                                            e.printStackTrace();
-                                            Snackbar.make(getSnackBarParentContainer(), getContext().getString(R.string.error_occurred_with_error_code,
-                                                    ((TwitterException) e).getErrorCode()), Snackbar.LENGTH_INDEFINITE)
-                                                    .setAction(R.string.retry, v -> onInitializeList())
-                                                    .show();
-                                        },
-                                        () -> {
-                                            getSwipeRefreshLayout().setRefreshing(false);
-                                            setProgressCircleLoading(false);
-                                        }
-                                )
-                );
-                break;
-            case PackageManager.PERMISSION_DENIED:
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 400);
-                    return;
-                }
-                break;
-            default:
-                getSwipeRefreshLayout().setRefreshing(false);
-                setProgressCircleLoading(false);
-        }
+        subscription.add(
+                getGeoLocationObservable()
+                        .concatMap(geolocation-> getResponseObservable(geolocation).subscribeOn(Schedulers.newThread()))
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                result-> {
+                                    list.clear();
+                                    list.addAll(Arrays.asList(result.getTrends()));
+                                    adapter.notifyDataSetChanged();
+                                },
+                                e -> {
+                                    e.printStackTrace();
+                                    Snackbar.make(getSnackBarParentContainer(), getContext().getString(R.string.error_occurred_with_error_code,
+                                            ((TwitterException) e).getErrorCode()), Snackbar.LENGTH_INDEFINITE)
+                                            .setAction(R.string.retry, v -> onInitializeList())
+                                            .show();
+                                },
+                                () -> {
+                                    getSwipeRefreshLayout().setRefreshing(false);
+                                    setProgressCircleLoading(false);
+                                }
+                        )
+        );
     }
 
     @Override
@@ -199,18 +174,15 @@ public class TrendsFragment extends BaseListFragment {
         return new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            onInitializeList();
-        }
-    }
-
-    public Observable<Trends> getResponseObservable(Location location) {
+    public Observable<Trends> getResponseObservable(GeoLocation geolocation) {
         return Observable.create(
                 subscriber->{
                     try {
-                        subscriber.onNext(getTrends(location));
+                        Trends trends = GlobalApplication.twitter
+                                .getPlaceTrends(GlobalApplication.twitter.getClosestTrends(geolocation).get(0).getWoeid());
+                        helper.setTrends(Arrays.asList(trends.getTrends()));
+
+                        subscriber.onNext(trends);
                         subscriber.onCompleted();
                     } catch (TwitterException e) {
                         subscriber.onError(e);
@@ -219,61 +191,22 @@ public class TrendsFragment extends BaseListFragment {
         );
     }
 
-    public Observable<Location> getLocationObservable(){
-        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        LocationListener[] locationListener = new LocationListener[1];
-        Observable<Location> result = Observable.create(
+    public Observable<GeoLocation> getGeoLocationObservable(){
+        return Observable.create(
                 subscriber -> {
                     try {
-                        Criteria criteria = new Criteria();
-                        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-                        criteria.setCostAllowed(false);
-                        String bestProvider = locationManager.getBestProvider(criteria, true);
-                        Location location = locationManager.getLastKnownLocation(bestProvider);
-                        if (location != null) {
-                            subscriber.onNext(location);
+                        Geocoder geocoder = new Geocoder(getContext());
+                        Locale locale = Locale.getDefault();
+                        Address address = geocoder.getFromLocationName(locale.getDisplayCountry(), 1).get(0);
+                        if (address.getCountryCode().equals(locale.getCountry())){
+                            subscriber.onNext(new GeoLocation(address.getLatitude(), address.getLongitude()));
                             subscriber.onCompleted();
-                        } else {
-                            locationListener[0] = new LocationListener() {
-                                @Override
-                                public void onLocationChanged(Location location) {
-                                    locationManager.removeUpdates(this);
-                                    subscriber.onNext(location);
-                                    subscriber.onCompleted();
-                                }
-
-                                @Override
-                                public void onStatusChanged(String s, int i, Bundle bundle) {}
-
-                                @Override
-                                public void onProviderEnabled(String s) {}
-
-                                @Override
-                                public void onProviderDisabled(String s) {}
-                            };
-                            locationManager.requestLocationUpdates(bestProvider, 0, 0, locationListener[0]);
                         }
-                    } catch(SecurityException e){
+                    } catch(IOException e){
                         subscriber.onError(e);
                     }
                 }
         );
-        result.doOnUnsubscribe(() -> {
-            if (locationListener[0] != null){
-                locationManager.removeUpdates(locationListener[0]);
-            }
-        });
-        return result;
-    }
-
-    private Trends getTrends(Location location) throws TwitterException {
-        Trends result = GlobalApplication.twitter
-                .getPlaceTrends(GlobalApplication.twitter.getClosestTrends(
-                        new GeoLocation(location.getLatitude(), location.getLongitude())
-                ).get(0).getWoeid());
-
-        helper.setTrends(Arrays.asList(result.getTrends()));
-        return result;
     }
 
 }
