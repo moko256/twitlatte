9c4d08b2d15da005ee9291730e7e5fb0a2b79716
moko256 <koutaro.mo@gmail.com>
2017-11-23 22:51:38 +0900

    bug fix

--- app/src/main/java/com/github/moko256/twicalico/BaseTweetListFragment.java
+++ app/src/main/java/com/github/moko256/twicalico/BaseTweetListFragment.java
@@ -181,14 +181,18 @@ public abstract class BaseTweetListFragment extends BaseListFragment {
         if (ids.size() - positions[0] > 1000){
             List<Long> list = ids.subList(positions[0] + 1000, ids.size());
             statusIdsDatabase.deleteIds(list);
-            boolean[] results = statusIdsDatabase.hasIdsOtherTable(list);
-            List<Long> deletableIds = new ArrayList<>();
-            for (int i = 0; i < list.size(); i++) {
-                if (!results[i]) {
-                    deletableIds.add(list.get(i));
+            try {
+                boolean[] results = statusIdsDatabase.hasIdsOtherTable(list);
+                List<Long> deletableIds = new ArrayList<>();
+                for (int i = 0; i < list.size(); i++) {
+                    if (!results[i]) {
+                        deletableIds.add(list.get(i));
+                    }
                 }
+                GlobalApplication.statusCache.delete(deletableIds);
+            } catch(Throwable e){
+                e.printStackTrace();
             }
-            GlobalApplication.statusCache.delete(deletableIds);
         }
     }
 
