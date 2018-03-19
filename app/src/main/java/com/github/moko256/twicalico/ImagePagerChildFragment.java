e3e0b25bba4e2e94ac6a74a0f8f5221684d6d8c9
moko256 <koutaro.mo@gmail.com>
2018-03-19 13:48:41 +0900

    refactor: fix bug

--- app/src/main/java/com/github/moko256/twicalico/ImagePagerChildFragment.java
+++ app/src/main/java/com/github/moko256/twicalico/ImagePagerChildFragment.java
@@ -269,7 +269,7 @@ public class ImagePagerChildFragment extends Fragment {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
     private void showSystemUI() {
