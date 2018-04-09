fe912727b3b62204a7f75147620b262038c4ed42
moko256 <koutaro.mo@gmail.com>
2018-04-10 03:48:26 +0900

    refactor: fix url parse bug (resolved #13)

--- app/src/main/java/com/github/moko256/twicalico/SplashActivity.java
+++ app/src/main/java/com/github/moko256/twicalico/SplashActivity.java
@@ -23,6 +23,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by moko256 on 2017/04/16.
@@ -35,9 +36,18 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = null;
+
+        startActivity(switchIntent());
+    }
+
+    private Intent switchIntent(){
+
+        if (GlobalApplication.twitter == null) {
+            return  new Intent(this, OAuthActivity.class);
+        }
 
         if (getIntent() != null){
+
             Bundle extras = getIntent().getExtras();
             if (extras != null){
                 if (extras.getCharSequence(Intent.EXTRA_TEXT) != null) {
@@ -47,45 +57,49 @@ public class SplashActivity extends AppCompatActivity {
                         text.append(subject).append(" ");
                     }
                     text.append(extras.getCharSequence(Intent.EXTRA_TEXT));
-                    intent = PostActivity.getIntent(
+                    return PostActivity.getIntent(
                             this,
                             -1,
                             text.toString()
                     );
                 } else if (extras.get(Intent.EXTRA_STREAM) != null) {
                     ArrayList<Uri> list = extras.getParcelableArrayList(Intent.EXTRA_STREAM);
-                    intent = PostActivity.getIntent(this, list);
+                    return PostActivity.getIntent(this, list);
                 }
-            } else if (getIntent().getData() != null){
-                Uri data = getIntent().getData();
+            }
+
+            Uri data = getIntent().getData();
+            if (data != null){
                 switch (data.getScheme()){
                     case "twitter":
                         switch (data.getHost()) {
                             case "post":
                                 String replyId = data.getQueryParameter("in_reply_to_status_id");
-                                intent = PostActivity.getIntent(
+                                return PostActivity.getIntent(
                                         this,
                                         replyId != null ? Long.valueOf(replyId) : -1,
                                         data.getQueryParameter("message")
                                 );
-                                break;
                             case "status":
-                                intent = ShowTweetActivity.getIntent(this, Long.parseLong(data.getQueryParameter("id")));
-                                break;
+                                return ShowTweetActivity.getIntent(this, Long.parseLong(data.getQueryParameter("id")));
                             case "user":
                                 String userId = data.getQueryParameter("id");
                                 if (userId != null){
-                                    intent = ShowUserActivity.getIntent(this, Long.valueOf(userId));
+                                    return ShowUserActivity.getIntent(this, Long.valueOf(userId));
                                 } else {
-                                    intent = ShowUserActivity.getIntent(this, data.getQueryParameter("screen_name"));
+                                    return ShowUserActivity.getIntent(this, data.getQueryParameter("screen_name"));
                                 }
-                                break;
                             default:
-                                intent = Intent.createChooser(new Intent(Intent.ACTION_VIEW, data), "");
+                                return Intent.createChooser(new Intent(Intent.ACTION_VIEW, data), "");
                         }
-                        break;
                     case "https":
-                        if (data.getPath().equals("/share") || data.getPath().equals("/intent/tweet")){
+                        List<String> pathSegments = data.getPathSegments();
+                        int size = pathSegments.size();
+
+                        String lastPathSegment = data.getLastPathSegment();
+
+                        if ((size == 1 && lastPathSegment.equals("share"))
+                                || (size == 2 && pathSegments.get(0).equals("intent") && lastPathSegment.equals("tweet"))){
                             StringBuilder tweet = new StringBuilder(data.getQueryParameter("text"));
                             if (data.getQueryParameter("url") != null){
                                 tweet.append(" ")
@@ -110,28 +124,27 @@ public class SplashActivity extends AppCompatActivity {
                                 }
                             }
 
-                            intent = PostActivity.getIntent(
+                            return PostActivity.getIntent(
                                     this,
                                     data.getQueryParameter("in-reply-to") != null
                                             ? Long.valueOf(data.getQueryParameter("in-reply-to"))
                                             :-1L,
                                     tweet.toString()
-                                    );
-                        } else if (data.getPath().equals("/search")){
-                            intent = SearchResultActivity.getIntent(this, data.getQueryParameter("q"));
-                        } else if (data.getPath().matches("/.+/status(es)*/.+")){
-                            intent = ShowTweetActivity.getIntent(this, Long.parseLong(data.getPathSegments().get(2)));
-                        } else if (data.getPathSegments().size() == 1){
-                            intent = ShowUserActivity.getIntent(this, data.getLastPathSegment());
+                            );
+                        } else if (size == 1 && lastPathSegment.equals("search")){
+                            return SearchResultActivity.getIntent(this, data.getQueryParameter("q"));
+                        } else if (size == 3 && (pathSegments.get(1).equals("status") || pathSegments.get(1).equals("statuses"))){
+                            return ShowTweetActivity.getIntent(this, Long.parseLong(pathSegments.get(2)));
+                        } else if (size == 1){
+                            return ShowUserActivity.getIntent(this, lastPathSegment);
                         } else if (data.getQueryParameter("status") != null){
-                            intent = PostActivity.getIntent(this, data.getQueryParameter("status"));
+                            return PostActivity.getIntent(this, data.getQueryParameter("status"));
                         } else {
-                            intent = Intent.createChooser(new Intent(Intent.ACTION_VIEW, data), "");
+                            return Intent.createChooser(new Intent(Intent.ACTION_VIEW, data), "");
                         }
-                        break;
                     case "web+mastodon":
                         if (data.getHost().equals("share")) {
-                            intent = PostActivity.getIntent(
+                            return PostActivity.getIntent(
                                     this,
                                     -1,
                                     data.getQueryParameter("text")
@@ -142,14 +155,6 @@ public class SplashActivity extends AppCompatActivity {
             }
         }
 
-        if (GlobalApplication.twitter == null) {
-            intent = new Intent(this, OAuthActivity.class);
-        }
-
-        if (intent == null) {
-            intent = new Intent(this, MainActivity.class);
-        }
-
-        startActivity(intent);
+        return new Intent(this, MainActivity.class);
     }
 }
