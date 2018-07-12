71beea8a298eb638b60dcfe7cf3fd84e9d07583e
moko256 <koutaro.mo@gmail.com>
2018-07-12 19:27:29 +0900

    fix: crash when destroy

--- app/src/main/java/com/github/moko256/twitlatte/BaseUsersFragment.java
+++ app/src/main/java/com/github/moko256/twitlatte/BaseUsersFragment.java
@@ -107,15 +107,15 @@ public abstract class BaseUsersFragment extends BaseListFragment {
 
     @Override
     public void onDestroyView() {
-        disposable.dispose();
         super.onDestroyView();
-        disposable = null;
         adapter=null;
     }
 
     @Override
     public void onDestroy() {
+        disposable.dispose();
         super.onDestroy();
+        disposable = null;
         list=null;
     }
 
