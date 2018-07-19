f88564aa5e7b96c1561f1c0b7063f035dd8a14c2
moko256 <koutaro.mo@gmail.com>
2018-07-19 22:43:24 +0900

    fix: crash when the toot has both gif and picture

--- app/src/main/java/com/github/moko256/twitlatte/database/CachedStatusesSQLiteOpenHelper.java
+++ app/src/main/java/com/github/moko256/twitlatte/database/CachedStatusesSQLiteOpenHelper.java
@@ -754,9 +754,9 @@ public class CachedStatusesSQLiteOpenHelper extends SQLiteOpenHelper {
                         mediaUrls[i],
                         mediaUrlHttpSs[i],
                         types[i],
-                        hasMedia? variants_bitrates[i]: null,
-                        hasMedia? variants_contentTypes[i]: null,
-                        hasMedia? variants_urls[i]: null,
+                        hasMedia &&  i< variants_bitrates.length ? variants_bitrates[i] : null,
+                        hasMedia &&  i< variants_contentTypes.length ? variants_contentTypes[i] : null,
+                        hasMedia &&  i< variants_urls.length ? variants_urls[i] : null,
                         starts[i],
                         ends[i]
                 );
