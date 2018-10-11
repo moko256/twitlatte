3b33323be407c27f6ac4719fae26fb0581be6fe8
moko256 <koutaro.mo@gmail.com>
2018-10-11 18:33:49 +0900

    feat: add new file

---
+++ app/src/main/java/com/github/moko256/twitlatte/widget/AdapterObservableBinder.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015-2018 The twitlatte authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.moko256.twitlatte.widget
+
+import androidx.recyclerview.widget.RecyclerView
+import com.github.moko256.twitlatte.entity.UpdateEvent
+
+/**
+ * Created by moko256 on 2018/10/11.
+ *
+ * @author moko256
+ */
+fun RecyclerView.Adapter<out RecyclerView.ViewHolder>.convertObservableConsumer(): (UpdateEvent) -> Unit {
+
+}
