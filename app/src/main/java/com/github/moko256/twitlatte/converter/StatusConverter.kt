fdcfffbd264bde81b2f7de077e9663f4e3c363bd
moko256 <koutaro.mo@gmail.com>
2018-10-15 02:29:19 +0900

    refactor: add converter

--- app/src/main/java/com/github/moko256/twitlatte/converter/StatusConverter.kt
+++ app/src/main/java/com/github/moko256/twitlatte/converter/StatusConverter.kt
@@ -16,6 +16,7 @@
 
 package com.github.moko256.twitlatte.converter
 
+import com.github.moko256.mastodon.MTStatus
 import com.github.moko256.twitlatte.entity.*
 import com.github.moko256.twitlatte.text.link.MTHtmlParser
 import com.github.moko256.twitlatte.text.link.convertToContentAndLinks
@@ -28,206 +29,282 @@ import com.sys1yagi.mastodon4j.api.entity.Attachment
  *
  * @author moko256
  */
-fun twitter4j.Status.convertToCommonStatus(): StatusObject {
-    return if (!isRetweet) {
-        val parsedSource: Pair<String, Array<Link>>? = if (source == null) {
-            null
-        } else {
-            MTHtmlParser.convertToContentAndLinks(source)
-        }
-        val urls = if (symbolEntities.isEmpty()
-                && hashtagEntities.isEmpty()
-                && userMentionEntities.isEmpty()
-                && mediaEntities.isEmpty()
-                && urlEntities.isEmpty()
-        ) {
-            null
-        } else {
-            convertToContentAndLinks(
-                    text = text,
-                    symbolEntities = symbolEntities,
-                    hashtagEntities = hashtagEntities,
-                    userMentionEntities = userMentionEntities,
-                    mediaEntities = mediaEntities,
-                    urlEntities = urlEntities
-            )
-
-        }
-
-        val mentions
-                = if (userMentionEntities.isNotEmpty()) {
-            userMentionEntities.map {
-                it.screenName
-            }.toTypedArray()
-        } else {
-            null
-        }
-
-        Status(
-                id = id,
-                userId = user.id,
-                text = urls?.first ?: text ?: "",
-                sourceName = parsedSource?.first,
-                sourceWebsite = parsedSource?.second?.first()?.url,
-                createdAt = createdAt,
-                inReplyToStatusId = inReplyToStatusId,
-                inReplyToUserId = inReplyToUserId,
-                inReplyToScreenName = inReplyToScreenName,
-                isFavorited = isFavorited,
-                isRepeated = isRetweeted,
-                favoriteCount = favoriteCount,
-                repeatCount = retweetCount,
-                repliesCount = 0,
-                isSensitive = isPossiblySensitive,
-                lang = lang,
-                medias = if (mediaEntities.isNotEmpty()) {
-                    mediaEntities.map {
-                        var downloadVideoUrl: String? = null
-                        var originalUrl: String? = null
-                        var type: String? = null
-
-                        when (it.type) {
-                            "video" -> {
-                                for (variant in it.videoVariants) {
-                                    if (variant.contentType == MimeTypes.APPLICATION_M3U8) {
-                                        originalUrl = variant.url
-                                        type = Media.ImageType.VIDEO_MULTI.value
-                                    } else if (variant.contentType == MimeTypes.VIDEO_MP4) {
-                                        downloadVideoUrl = variant.url
-                                    }
-                                }
-
-                                if (downloadVideoUrl == null) {
-                                    originalUrl = it.videoVariants[0].url
-                                    type = Media.ImageType.VIDEO_ONE.value
-                                }
-                            }
-                            "animated_gif" -> {
-                                originalUrl = it.videoVariants[0].url
-                                type = Media.ImageType.GIF.value
-                            }
-                            else -> {
-                                originalUrl = null
-                                type = Media.ImageType.PICTURE.value
-                            }
-                        }
-
-                        Media(
-                                thumbnailUrl = if (originalUrl != null) {
-                                    it.mediaURLHttps
-                                } else {
-                                    null
-                                },
-                                downloadVideoUrl = downloadVideoUrl,
-                                originalUrl = originalUrl ?: it.mediaURLHttps,
-                                imageType = type
-                                        ?: Media.ImageType.PICTURE.value
-                        )
-                    }.toTypedArray()
-                } else {
-                    null
-                },
-                urls = urls?.second,
-                mentions = mentions,
-                emojis = null,
-                url = "https://twitter.com/" + user.screenName + "/status/" + id.toString(),
-                spoilerText = null,
-                quotedStatusId = quotedStatusId,
-                visibility = null
-        )
+fun twitter4j.Status.convertToPost(): Post {
+    return if (this is MTStatus) {
+        this.status.convertToCommonStatus()
     } else {
-        Repeat(
-                id = id,
-                userId = user.id,
-                repeatedStatusId = retweetedStatus.id,
-                createdAt = createdAt
-        )
+        this.convertToCommonStatus()
     }
 }
 
-fun com.sys1yagi.mastodon4j.api.entity.Status.convertToCommonStatus(): StatusObject {
-    val urls = MTHtmlParser.convertToContentAndLinks(text = content)
 
-    return if (reblog == null) {
-        Status(
-                id = id,
-                userId = account.id,
-                text = urls.first,
-                sourceName = application?.name,
-                sourceWebsite = application?.website,
-                createdAt = ISO8601DateConverter.toDate(createdAt),
-                inReplyToStatusId = inReplyToId.convertIfZero(),
-                inReplyToUserId = inReplyToAccountId.convertIfZero(),
-                inReplyToScreenName = if (inReplyToAccountId != 0L) {
-                    ""
-                } else {
-                    null
-                },
-                isFavorited = isFavourited,
-                isRepeated = isReblogged,
-                favoriteCount = favouritesCount,
-                repeatCount = reblogsCount,
-                repliesCount = repliesCount,
-                isSensitive = isSensitive,
-                lang = language,
-                medias = if (mediaAttachments.isNotEmpty()) {
-                    mediaAttachments.map {
-                        val resultUrl = it.url
-                        val thumbnailUrl: String?
-                        val type: Media.ImageType
+fun twitter4j.Status.convertToCommonStatus(): Post {
+    val status: Status
+    val statusUser: User
+    val repeat: Repeat?
+    val repeatUser: User?
+    val quotedStatus: Status?
+    val quotedStatusUser: User?
 
-                        when (it.type) {
-                            Attachment.Type.Video.value -> {
-                                thumbnailUrl = it.previewUrl
-                                type = Media.ImageType.VIDEO_ONE
+    val baseRepeat = retweetedStatus
+    if (baseRepeat == null) {
+        status = convertToStatus()
+        statusUser = user.convertToCommonUser()
+        repeat = null
+        repeatUser = null
+        quotedStatus = getQuotedStatus().convertToStatus()
+        quotedStatusUser = getQuotedStatus().user.convertToCommonUser()
+    } else {
+        repeat = convertToRepeat()
+        repeatUser = user.convertToCommonUser()
+
+        status = baseRepeat.convertToStatus()
+        statusUser = baseRepeat.user.convertToCommonUser()
+
+        quotedStatus = baseRepeat.quotedStatus.convertToStatus()
+        quotedStatusUser = baseRepeat.quotedStatus.user.convertToCommonUser()
+    }
+
+    return Post(
+            id = id,
+            status = status,
+            user = statusUser,
+            repeat = repeat,
+            repeatedUser = repeatUser,
+            quotedRepeatingStatus = quotedStatus,
+            quotedRepeatingUser = quotedStatusUser
+    )
+}
+
+private fun twitter4j.Status.convertToStatus(): Status {
+    val parsedSource: Pair<String, Array<Link>>? = if (source == null) {
+        null
+    } else {
+        MTHtmlParser.convertToContentAndLinks(source)
+    }
+    val urls = if (symbolEntities.isEmpty()
+            && hashtagEntities.isEmpty()
+            && userMentionEntities.isEmpty()
+            && mediaEntities.isEmpty()
+            && urlEntities.isEmpty()
+    ) {
+        null
+    } else {
+        convertToContentAndLinks(
+                text = text,
+                symbolEntities = symbolEntities,
+                hashtagEntities = hashtagEntities,
+                userMentionEntities = userMentionEntities,
+                mediaEntities = mediaEntities,
+                urlEntities = urlEntities
+        )
+
+    }
+
+    val mentions
+            = if (userMentionEntities.isNotEmpty()) {
+        userMentionEntities.map {
+            it.screenName
+        }.toTypedArray()
+    } else {
+        null
+    }
+
+    return Status(
+            id = id,
+            userId = user.id,
+            text = urls?.first ?: text ?: "",
+            sourceName = parsedSource?.first,
+            sourceWebsite = parsedSource?.second?.first()?.url,
+            createdAt = createdAt,
+            inReplyToStatusId = inReplyToStatusId,
+            inReplyToUserId = inReplyToUserId,
+            inReplyToScreenName = inReplyToScreenName,
+            isFavorited = isFavorited,
+            isRepeated = isRetweeted,
+            favoriteCount = favoriteCount,
+            repeatCount = retweetCount,
+            repliesCount = 0,
+            isSensitive = isPossiblySensitive,
+            lang = lang,
+            medias = if (mediaEntities.isNotEmpty()) {
+                mediaEntities.map {
+                    var downloadVideoUrl: String? = null
+                    var originalUrl: String? = null
+                    var type: String? = null
+
+                    when (it.type) {
+                        "video" -> {
+                            for (variant in it.videoVariants) {
+                                if (variant.contentType == MimeTypes.APPLICATION_M3U8) {
+                                    originalUrl = variant.url
+                                    type = Media.ImageType.VIDEO_MULTI.value
+                                } else if (variant.contentType == MimeTypes.VIDEO_MP4) {
+                                    downloadVideoUrl = variant.url
+                                }
                             }
-                            Attachment.Type.Gifv.value -> {
-                                thumbnailUrl = it.previewUrl
-                                type = Media.ImageType.GIF
-                            }
-                            else -> {
-                                thumbnailUrl = null
-                                type = Media.ImageType.PICTURE
+
+                            if (downloadVideoUrl == null) {
+                                originalUrl = it.videoVariants[0].url
+                                type = Media.ImageType.VIDEO_ONE.value
                             }
                         }
+                        "animated_gif" -> {
+                            originalUrl = it.videoVariants[0].url
+                            type = Media.ImageType.GIF.value
+                        }
+                        else -> {
+                            originalUrl = null
+                            type = Media.ImageType.PICTURE.value
+                        }
+                    }
 
-                        Media(
-                                thumbnailUrl = thumbnailUrl,
-                                originalUrl = resultUrl,
-                                imageType = type.value
-                        )
-                    }.toTypedArray()
-                } else {
-                    null
-                },
-                urls = urls.second,
-                mentions = mentions.map {
-                    it.acct
-                }.toTypedArray(),
-                emojis = if (emojis.isNotEmpty()) {
-                    emojis.map {
-                        Emoji(url = it.url, shortCode = it.shortcode)
-                    }.toTypedArray()
-                } else {
-                    null
-                },
-                url = url,
-                spoilerText = if (spoilerText.isNotEmpty()) {
-                    spoilerText
-                } else {
-                    null
-                },
-                quotedStatusId = -1,
-                visibility = visibility
-        )
+                    Media(
+                            thumbnailUrl = if (originalUrl != null) {
+                                it.mediaURLHttps
+                            } else {
+                                null
+                            },
+                            downloadVideoUrl = downloadVideoUrl,
+                            originalUrl = originalUrl ?: it.mediaURLHttps,
+                            imageType = type
+                                    ?: Media.ImageType.PICTURE.value
+                    )
+                }.toTypedArray()
+            } else {
+                null
+            },
+            urls = urls?.second,
+            mentions = mentions,
+            emojis = null,
+            url = "https://twitter.com/" + user.screenName + "/status/" + id.toString(),
+            spoilerText = null,
+            quotedStatusId = quotedStatusId,
+            visibility = null
+    )
+}
+
+private fun twitter4j.Status.convertToRepeat(): Repeat {
+    return Repeat(
+            id = id,
+            userId = user.id,
+            repeatedStatusId = retweetedStatus.id,
+            createdAt = createdAt
+    )
+}
+
+fun com.sys1yagi.mastodon4j.api.entity.Status.convertToCommonStatus(): Post {
+    val status: Status
+    val statusUser: User
+    val repeat: Repeat?
+    val repeatUser: User?
+
+    val baseRepeat = reblog
+    if (baseRepeat == null) {
+        status = convertToStatus()
+        statusUser = account.convertToCommonUser()
+        repeat = null
+        repeatUser = null
     } else {
-        Repeat(
-                id = id,
-                userId = account.id,
-                repeatedStatusId = reblog!!.id,
-                createdAt = ISO8601DateConverter.toDate(createdAt)
-        )
+        repeat = convertToRepeat()
+        repeatUser = account.convertToCommonUser()
+
+        status = baseRepeat.convertToStatus()
+        statusUser = baseRepeat.account.convertToCommonUser()
     }
+
+    return Post(
+            id = id,
+            status = status,
+            user = statusUser,
+            repeat = repeat,
+            repeatedUser = repeatUser
+    )
+}
+
+private fun com.sys1yagi.mastodon4j.api.entity.Status.convertToStatus(): Status {
+    val urls = MTHtmlParser.convertToContentAndLinks(text = content)
+
+    return Status(
+            id = id,
+            userId = account.id,
+            text = urls.first,
+            sourceName = application?.name,
+            sourceWebsite = application?.website,
+            createdAt = ISO8601DateConverter.toDate(createdAt),
+            inReplyToStatusId = inReplyToId.convertIfZero(),
+            inReplyToUserId = inReplyToAccountId.convertIfZero(),
+            inReplyToScreenName = if (inReplyToAccountId != 0L) {
+                ""
+            } else {
+                null
+            },
+            isFavorited = isFavourited,
+            isRepeated = isReblogged,
+            favoriteCount = favouritesCount,
+            repeatCount = reblogsCount,
+            repliesCount = repliesCount,
+            isSensitive = isSensitive,
+            lang = language,
+            medias = if (mediaAttachments.isNotEmpty()) {
+                mediaAttachments.map {
+                    val resultUrl = it.url
+                    val thumbnailUrl: String?
+                    val type: Media.ImageType
+
+                    when (it.type) {
+                        Attachment.Type.Video.value -> {
+                            thumbnailUrl = it.previewUrl
+                            type = Media.ImageType.VIDEO_ONE
+                        }
+                        Attachment.Type.Gifv.value -> {
+                            thumbnailUrl = it.previewUrl
+                            type = Media.ImageType.GIF
+                        }
+                        else -> {
+                            thumbnailUrl = null
+                            type = Media.ImageType.PICTURE
+                        }
+                    }
+
+                    Media(
+                            thumbnailUrl = thumbnailUrl,
+                            originalUrl = resultUrl,
+                            imageType = type.value
+                    )
+                }.toTypedArray()
+            } else {
+                null
+            },
+            urls = urls.second,
+            mentions = mentions.map {
+                it.acct
+            }.toTypedArray(),
+            emojis = if (emojis.isNotEmpty()) {
+                emojis.map {
+                    Emoji(url = it.url, shortCode = it.shortcode)
+                }.toTypedArray()
+            } else {
+                null
+            },
+            url = url,
+            spoilerText = if (spoilerText.isNotEmpty()) {
+                spoilerText
+            } else {
+                null
+            },
+            quotedStatusId = -1,
+            visibility = visibility
+    )
+}
+
+private fun com.sys1yagi.mastodon4j.api.entity.Status.convertToRepeat(): Repeat {
+    return Repeat(
+            id = id,
+            userId = account.id,
+            repeatedStatusId = reblog!!.id,
+            createdAt = ISO8601DateConverter.toDate(createdAt)
+    )
 }
 
 private fun Long.convertIfZero() = if (this == 0L){
