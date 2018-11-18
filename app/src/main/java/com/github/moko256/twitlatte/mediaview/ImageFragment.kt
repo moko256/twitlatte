711bddf913c77b90ccb3365072b0ae591f2274bc
moko256 <koutaro.mo@gmail.com>
2018-11-18 17:45:48 +0900

    feat: new image view

--- app/src/main/java/com/github/moko256/twitlatte/mediaview/ImageFragment.kt
+++ app/src/main/java/com/github/moko256/twitlatte/mediaview/ImageFragment.kt
@@ -16,11 +16,16 @@
 
 package com.github.moko256.twitlatte.mediaview
 
+import android.graphics.Bitmap
+import android.graphics.PointF
 import android.os.Bundle
 import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.github.chrisbanes.photoview.PhotoView
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+import com.davemorrissey.labs.subscaleview.ImageSource
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import com.github.chuross.flinglayout.FlingLayout
 import com.github.moko256.twitlatte.GlobalApplication
 import com.github.moko256.twitlatte.R
@@ -35,7 +40,7 @@ import com.github.moko256.twitlatte.text.TwitterStringUtils
  */
 class ImageFragment: AbstractMediaFragment() {
 
-    private lateinit var imageView: PhotoView
+    private lateinit var imageView: SubsamplingScaleImageView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -54,24 +59,43 @@ class ImageFragment: AbstractMediaFragment() {
                 showSystemUI()
             }
         }
-        imageView.setOnScaleChangeListener { scaleFactor: Float, _: Float, _: Float ->
-            (view as FlingLayout).isDragEnabled = scaleFactor <= 1f
-        }
+        imageView.maxScale = 4F
+        imageView.setMinimumDpi(100)
+        imageView.setDoubleTapZoomDpi(100)
+        imageView.setDoubleTapZoomDuration(250)
+        imageView.setOnStateChangedListener(object: SubsamplingScaleImageView.OnStateChangedListener {
+            override fun onCenterChanged(newCenter: PointF?, origin: Int) {}
+
+            override fun onScaleChanged(newScale: Float, origin: Int) {
+                (view as FlingLayout).isDragEnabled = newScale <= imageView.minScale
+            }
+        })
+
         val requests = GlideApp.with(this)
         val url = media.originalUrl
 
         requests
+                .asBitmap()
                 .load(TwitterStringUtils.convertLargeImageUrl(url))
                 .fitCenter()
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .thumbnail(requests.load(
+                .thumbnail(requests.asBitmap().load(
                         if (GlobalApplication.preferenceRepository.getString(KEY_TIMELINE_IMAGE_LOAD_MODE, "normal") == "normal") {
                             TwitterStringUtils.convertSmallImageUrl(url)
                         } else {
                             TwitterStringUtils.convertThumbImageUrl(url)
                         }
                 ))
-                .into(imageView)
+                .into(object: SimpleTarget<Bitmap>() {
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        imageView.setImage(ImageSource.cachedBitmap(resource))
+                    }
+                })
+    }
+
+    override fun onDestroyView() {
+        imageView.recycle()
+        super.onDestroyView()
     }
 
     override fun returnLayoutId() = R.layout.fragment_image
