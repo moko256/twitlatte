b564626849182f730e6246c501524a662fa1aed4
moko256 <koutaro.mo@gmail.com>
2019-06-16 22:25:03 +0900

    chore: disable animation

--- app/src/main/java/com/github/moko256/twitlatte/MainActivity.java
+++ app/src/main/java/com/github/moko256/twitlatte/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements
 
         if (drawer != null){
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            drawer.addDrawerListener(toggle);
+            toggle.setDrawerSlideAnimationEnabled(false);
             toggle.syncState();
 
             drawer.addDrawerListener(this);
