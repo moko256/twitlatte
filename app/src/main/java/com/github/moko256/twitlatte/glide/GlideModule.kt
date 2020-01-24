4bd11401c3dd4bea1329ccbe188be470a650fc5f
moko256 <koutaro.mo@gmail.com>
2020-01-24 12:05:22 +0900

    refactor: reduce disk cache size

--- app/src/main/java/com/github/moko256/twitlatte/glide/GlideModule.kt
+++ app/src/main/java/com/github/moko256/twitlatte/glide/GlideModule.kt
@@ -25,6 +25,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
@@ -48,16 +49,18 @@ class GlideModule : AppGlideModule() {
             val padding = context.dpToPx(8)
             DrawableCompat.setTint(error, ContextCompat.getColor(context, R.color.color_accent))
             error = InsetDrawable(error, padding)
-            builder.setDefaultRequestOptions(RequestOptions().error(error)
+            builder.setDiskCache(InternalCacheDiskCacheFactory(context, 50 * 1024 * 1024))
+            builder.setDefaultRequestOptions(
+                RequestOptions().error(error)
             )
         }
     }
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         registry.replace(
-                GlideUrl::class.java,
-                InputStream::class.java,
-                OkHttpUrlLoader.Factory(appOkHttpClientInstance)
+            GlideUrl::class.java,
+            InputStream::class.java,
+            OkHttpUrlLoader.Factory(appOkHttpClientInstance)
         )
     }
 
